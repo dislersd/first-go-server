ac6e7f9de011e2265c8017e3280a194956ca5b92
dislersd <dislersd@gmail.com>
2019-04-05 21:54:43 -0700

    built handler functions

--- rest.go
+++ rest.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"github.com/julienschmidt/httprouter"
 	"log"
 	"net/http"
 )
@@ -22,4 +24,21 @@ func main() {
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
+}
+
+func show(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	k := p.ByName("key")
+	if k == "" {
+		fmt.Fprint(w, "Read list: %v", data)
+		return
+	}
+	fmt.Fprint(w, "Read entry: data[%s] = %s", k, data[k])
+}
+
+func update(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	k := p.ByName("key")
+	v := p.ByName("value")
+
+	data[k] = v
+	fmt.Fprint(w, "Updated: data[%s] = %s", k, data[k])
 }
